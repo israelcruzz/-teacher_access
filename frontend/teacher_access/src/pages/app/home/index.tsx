3730e0869aaf8ba6ca1cc80a36478db3d4faad29
israelcruzz <israelcruz.contato@gmail.com>
2024-07-26 20:55:54 -0300

    add table in home page with fake date

--- frontend/teacher_access/src/pages/app/home/index.tsx
+++ frontend/teacher_access/src/pages/app/home/index.tsx
@@ -1,16 +1,132 @@
-import { Search } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Plus, Search, Ellipsis } from "lucide-react";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 export const Home = () => {
   return (
-    <main className="w-full py-8 px-6">
+    <main className="realative w-full py-8 px-6 flex flex-col">
       <header className="flex justify-between items-center">
         <h1 className="text-2xl font-bold">Students</h1>
 
-        <div className="flex w-[300px] h-10 rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 items-center gap-2">
-          <Search className="h-4 w-4" />
-          <input type="text" className="bg-transparent outline-none" placeholder="Search student..." />
+        <div className="flex gap-4">
+          <Button variant={"secondary"}>Add Student</Button>
+          <Select>
+            <SelectTrigger className="w-[180px]">
+              <SelectValue placeholder="Course" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="all">All</SelectItem>
+              <SelectItem value="light">Light</SelectItem>
+              <SelectItem value="dark">Dark</SelectItem>
+              <SelectItem value="system">System</SelectItem>
+            </SelectContent>
+          </Select>
+
+          <div className="flex w-[300px] h-10 rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 items-center gap-2">
+            <Search className="h-4 w-4" />
+            <input
+              type="text"
+              className="bg-transparent outline-none flex-1"
+              placeholder="Search student..."
+            />
+          </div>
         </div>
       </header>
+
+      <div className="mt-6 rounded-md border">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Name</TableHead>
+              <TableHead>Email</TableHead>
+              <TableHead>Course</TableHead>
+              <TableHead>CreatedAt</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {Array.from({ length: 5 }).map((_, i) => {
+              return (
+                <TableRow key={i}>
+                  <TableCell className="font-medium">Israel Cruz</TableCell>
+                  <TableCell>israel@dev.com</TableCell>
+                  <TableCell>TI</TableCell>
+                  <TableCell>24/03/2020</TableCell>
+                  <TableCell>
+                    <DropdownMenu>
+                      <DropdownMenuTrigger asChild>
+                        <button className="flex justify-center items-center  rounded-md p-1">
+                          <Ellipsis />
+                        </button>
+                      </DropdownMenuTrigger>
+                      <DropdownMenuContent>
+                        <DropdownMenuLabel>Israel Cruz</DropdownMenuLabel>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem>Edit</DropdownMenuItem>
+                        <DropdownMenuItem>Delete</DropdownMenuItem>
+                      </DropdownMenuContent>
+                    </DropdownMenu>
+                  </TableCell>
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </div>
+
+      <div className="self-end mt-2">
+        <Pagination>
+          <PaginationContent>
+            <PaginationItem>
+              <PaginationPrevious href="#" />
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationLink href="#">1</PaginationLink>
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationEllipsis />
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationNext href="#" />
+            </PaginationItem>
+          </PaginationContent>
+        </Pagination>
+      </div>
+
+      <Button className="fixed bottom-0 right-0 m-6">
+        <Plus size={16} />
+      </Button>
     </main>
   );
 };
